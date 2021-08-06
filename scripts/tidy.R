b24c6984221951947ec314070ffbae876c408d91
Rene Dario Herrera <56937719+UACC-renedherrera@users.noreply.github.com>
2021-08-06 14:32:24 -0700

    read data and inspect

--- scripts/tidy.R
+++ scripts/tidy.R
@@ -4,6 +4,12 @@
 # renedherrera at email dot arizona dot edu 
 # 6 Aug 2021
 
+# source info 
+# Vaccination Coverage among Adolescents (13-17 Years)
+# Data on adolescent vaccination coverage and selected sociodemographic characteristics by State, HHS Region, and the United States from the National Immunization Survey-Teen (NIS-Teen).
+# Additional information available at https://www.cdc.gov/vaccines/imz-managers/coverage/teenvaxview/index.html
+# https://data.cdc.gov/Teen-Vaccinations/Vaccination-Coverage-among-Adolescents-13-17-Years/ee48-w5t6
+
 # setup 
 library(here)
 library(RSocrata)
@@ -11,3 +17,17 @@ library(janitor)
 library(tidyverse)
 
 # read data 
+vac_data <- read.socrata(url = "https://data.cdc.gov/resource/ee48-w5t6.json") %>%
+  clean_names()
+
+# inspect 
+glimpse(vac_data)
+
+# geography type
+vac_data %>%
+  count(geography_type)
+
+# geography
+vac_data %>%
+  count(geography)
+
