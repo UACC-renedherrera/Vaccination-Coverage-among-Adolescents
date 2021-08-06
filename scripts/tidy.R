3e3408b929d7dc156d5b0cb03db984bc710a3dcb
Rene Dario Herrera <56937719+UACC-renedherrera@users.noreply.github.com>
2021-08-06 14:47:14 -0700

    inspect

--- scripts/tidy.R
+++ scripts/tidy.R
@@ -10,17 +10,18 @@
 # Additional information available at https://www.cdc.gov/vaccines/imz-managers/coverage/teenvaxview/index.html
 # https://data.cdc.gov/Teen-Vaccinations/Vaccination-Coverage-among-Adolescents-13-17-Years/ee48-w5t6
 
-# setup 
+# setup ####
 library(here)
 library(RSocrata)
 library(janitor)
 library(tidyverse)
 
-# read data 
+# read data ####
 vac_data <- read.socrata(url = "https://data.cdc.gov/resource/ee48-w5t6.json") %>%
-  clean_names()
+  clean_names() %>%
+  as_tibble()
 
-# inspect 
+# inspect ####
 glimpse(vac_data)
 
 # geography type
@@ -31,3 +32,28 @@ vac_data %>%
 vac_data %>%
   count(geography)
 
+# year 
+vac_data %>%
+  count(year_season)
+
+# dimension type
+vac_data %>%
+  count(dimension_type)
+
+# dimension
+vac_data %>%
+  count(dimension)
+
+# tidy ####
+# change character variables to numeric 
+vac_data <- vac_data %>%
+  mutate(
+    coverage_estimate = as.numeric(coverage_estimate),
+    population_sample_size = as.numeric(population_sample_size)
+  )
+
+# need to parse the 95 CI to lower and upper 
+
+
+# save to disk ####
+write_rds(vac_data, "data/tidy/vaccination_data.rds")
