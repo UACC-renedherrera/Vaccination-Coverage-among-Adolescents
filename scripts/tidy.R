ba995569e23ab0f0eee4a4ac3845df6ec4931c86
Rene Dario Herrera <56937719+UACC-renedherrera@users.noreply.github.com>
2021-08-06 14:23:21 -0700

    new file

---
+++ scripts/tidy.R
@@ -0,0 +1,13 @@
+# HPV vaccination rates? 
+# René Dario Herrera 
+# The University of Arizona Cancer Center 
+# renedherrera at email dot arizona dot edu 
+# 6 Aug 2021
+
+# setup 
+library(here)
+library(RSocrata)
+library(janitor)
+library(tidyverse)
+
+# read data 
